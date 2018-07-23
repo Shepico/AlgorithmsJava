488ac774facbaad45b47bab59045f01f73c0d353
Shepico <Shepico@gmail.com>
2018-07-23 12:27:16 +0400

    Реализация односвязного списка

---
+++ lesson4/src/ListOneS.java
@@ -0,0 +1,95 @@
+public class ListOneS implements List {
+
+    private ListItem first;
+
+    public ListOneS() {
+        first = null;
+    }
+
+    @Override
+    public void insertFirst(int value) {
+        ListItem newItem = new ListItem(value, this.first);
+        this.first = newItem;
+    }
+
+    @Override
+    public int deleteFirst() {
+        int deleteItem = this.first.getData();
+        this.first = this.first.getNextItem();
+        return (deleteItem);
+    }
+
+    @Override
+    public void display() {
+        ListItem link;
+        if (!isEmpty()) {
+            link = this.first;
+            while (link != null) {
+                System.out.println(link.getData());
+                link = link.getNextItem();
+            }
+        }else System.out.println("List is empty");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.first == null ? true: false;
+    }
+
+    @Override
+    public int findValue(int value) {
+        ListItem link;
+        if (isEmpty()) {
+            System.out.println("List is empty");
+            return 0;
+        }else {
+            link = this.first;
+            while (link != null) {
+                if (link.getData() == value) {
+                    return link.getData();
+                }
+                link = link.getNextItem();
+            }
+            return 0;
+        }
+    }
+
+    @Override
+    public int deleteValue(int value) {
+        // не стал делать через Delete, чтобы не усложнять тот метод
+        ListItem link;
+        ListItem nextLink;
+        ListItem prevLink;
+        if (isEmpty()) {
+            System.out.println("List is empty");
+            return 0;
+        }else {
+            link = this.first;
+            prevLink = link;
+            while (link != null) {
+                if (link.getData() == value) {
+                   if (link == first) {
+                       deleteFirst();
+                   }else {
+                       prevLink.setNextItem(link.getNextItem());
+                   }
+
+                }
+                //prevLink = link;
+                link = link.getNextItem();
+            }
+            return 0;
+        }
+    }
+
+    @Override
+    public void insertLast(int value) {
+        System.out.println("The method is not implemented");
+    }
+
+    @Override
+    public int deleteLast() {
+        System.out.println("The method is not implemented");
+        return 0;
+    }
+}
--- lesson4/src/Main.java
+++ lesson4/src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main (String[] args) {
+        List listOne = new ListOneS();
+        listOne.display();
+        listOne.insertFirst(5);
+        listOne.insertFirst(3);
+        listOne.insertFirst(4);
+        listOne.display();
+        listOne.insertLast(5);
+        System.out.println("Delete - " + listOne.findValue(3));
+        listOne.deleteValue(3);
+        listOne.display();
 
     }
 }
