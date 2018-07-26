6c0efc71a82157531bc1434183ab868155820997
Shepico <Shepico@gmail.com>
2018-07-26 14:56:17 +0400

    Создан рюкзак. Считаю стоимость. Но не выходит загрузка вещами?

--- lesson5/src/backpack/Backpack.java
+++ lesson5/src/backpack/Backpack.java
@@ -1,20 +1,39 @@
 package backpack;
 
+import java.util.ArrayList;
+
 class Backpack {
     private int capacity;
-   // Kit kitMax;
+    ArrayList<Thing> set;
 
     Backpack (int capacity) {
         this.capacity = capacity;
-     //   this.kitMax = new Kit();
+        set = new ArrayList();
     }
 
     Backpack () {
         this.capacity = 20;
-       // this.kitMax = new Kit();
     }
 
     int getCapacity() {
         return this.capacity;
+    }
+
+    void addThing(Thing current){
+        set.add(current);
+    }
+
+    void removeThing(Thing current) {
+        set.remove(current);
+    }
+
+    @Override
+    public String toString() {
+        String str = "Backpack [";
+        for (int i = 0; i<set.size(); i++){
+            str = str + set.get(i) + ",";
+        }
+        str = str + "]";
+        return (str);
     }
 }
--- lesson5/src/backpack/MainClass.java
+++ lesson5/src/backpack/MainClass.java
@@ -1,30 +1,51 @@
 package backpack;
 
 class MainClass {
+    static Thing[] arr = new Thing[5];
+    static Backpack bp = new Backpack(9);
+    static int maxCost;
+    static int fullCost;
+    static int capacity;
 
     public static void main (String[] args){
-        Thing[] arr = new Thing[5];
+        //заполниои массив вещами
         arr[0] = new Thing("book", 1,6);
         arr[1] = new Thing("binoculars", 2,5);
         arr[2] = new Thing("aid_kit", 4,7);
         arr[3] = new Thing("notebook", 5,4);
         arr[4] = new Thing("pot", 1,3);
 
-        System.out.println(arr[1]);
-
-        Backpack bp = new Backpack(7);
-        selectThing(arr, bp.getCapacity());
-    }
-
-    static void selectThing (Thing[] arr, int capacity) {
-        int maxCost=0;
-        int maxWeight = 0;
-        for (int i=0; i<arr.length & maxWeight < capacity; i++){
-            maxCost = maxCost + arr[i].getCost();
-            maxWeight = maxWeight + arr[i].getWeight();
+        //подсчитали стоимость вcех вещей
+        for (int i=0; i<arr.length; i++){
+            fullCost += arr[i].getCost();
         }
-        System.out.println(maxCost + " " + maxWeight);
 
+        maxCost = 0; //Установили максимальную стоимость
+        capacity = bp.getCapacity(); //запомнили грузоподъемность рюкзака
+        int res = selectT (0,0, fullCost);
+        System.out.println(res);
+        //System.out.println(bp);
     }
 
+
+    static int selectT (int i, int currentWeight, int fullCost){
+        //Включаем предмет
+        if (currentWeight + arr[i].getWeight() <= capacity ) {
+            if (i < arr.length - 1) {
+                selectT(i+1, currentWeight + arr[i].getWeight(), fullCost);
+            } else if (fullCost > maxCost) {
+                maxCost = fullCost;
+            }
+        }
+        //Исключаем предмет
+        if (fullCost > maxCost + arr[i].getCost()) {
+
+            if (i < arr.length - 1) {
+                selectT(i+1, currentWeight, fullCost - arr[i].getCost());
+            }else {
+                maxCost = fullCost - arr[i].getCost();
+            }
+        }
+        return maxCost;
+    }
 }
