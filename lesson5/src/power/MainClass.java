a766556f4352fb3708bcf45ffa7fb2c08086710b
Shepico <Shepico@gmail.com>
2018-07-26 11:09:03 +0400

    реализовано возведение в степень

--- lesson5/src/power/MainClass.java
+++ lesson5/src/power/MainClass.java
@@ -1,9 +1,29 @@
 package power;
 
- class MainClass {
+import java.util.Scanner;
+
+class MainClass {
+        static Scanner sc = new Scanner(System.in);
 
     public static void main (String[] args){
+        int number;
+        int pow;
 
+        System.out.print("Введите число - ");
+        number = sc.nextInt();
+
+        System.out.print("Введите степень - ");
+        pow = sc.nextInt();
+
+        System.out.println("Result - " + elevate (1, number, pow));
+
+    }
+
+    static int elevate (int result, int num, int p) {
+        if (p == 0) {
+            return result;
+        }
+        return num*elevate (result, num, --p);
     }
 
 }
