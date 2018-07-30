f4858e40c8006952909150cda56255860a4d1b3f
Shepico <Shepico@gmail.com>
2018-07-30 10:46:56 +0400

    Рюкзак в DZ чужой. Разобрался. Рекрсия в цикле страшная вещь :)

--- lesson5/src/backpack/Backpack.java
+++ lesson5/src/backpack/Backpack.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 class Backpack {
     private int capacity;
     ArrayList<Thing> set;
+    private int maxPrice = 0;
 
     Backpack (int capacity) {
         this.capacity = capacity;
@@ -27,7 +28,50 @@ class Backpack {
         set.remove(current);
     }
 
-   void pickupThing2 (Thing[] arr, int cw, int cc, ArrayList<Thing> cArr, ArrayList<Thing> mArr) {
+    void bestSet (ArrayList<Thing> arr) {
+        if (set.size() == 0) {
+            if (calcWeight(arr) <= capacity) {
+                set = arr;
+                maxPrice = calcPrice(arr);
+            }
+
+        }
+
+        if (calcWeight(arr)<=capacity && calcPrice(arr) > maxPrice) {
+            set = arr;
+        }
+    }
+
+
+    int calcWeight (ArrayList<Thing> arr) {
+        int currentWeight = 0;
+        for (Thing i : arr) {
+            currentWeight += i.getWeight();
+        }
+        return currentWeight;
+    }
+
+    int calcPrice (ArrayList<Thing> arr) {
+        int currentPrice = 0;
+        for (Thing p : arr) {
+            currentPrice += p.getCost();
+        }
+        return currentPrice;
+    }
+
+    void pickupThing (ArrayList arr) {
+        if (arr.size()>0) {
+            bestSet(arr);
+        }
+
+        for (int i = 0; i < arr.size(); i++){
+            ArrayList <Thing> list = new ArrayList<>(arr);
+            list.remove(i);
+            pickupThing (list);
+        }
+    }
+
+    void pickupThing2 (Thing[] arr, int cw, int cc, ArrayList<Thing> cArr, ArrayList<Thing> mArr) {
         int maxC = 0;
         int maxW = 0;
         for (int a = 0; a < arr.length; a++) {
@@ -112,7 +156,7 @@ class Backpack {
         for (int i = 0; i<set.size(); i++){
             str = str + set.get(i) + ",";
         }
-        str = str + "]";
+        str = str + "] Best = "+this.maxPrice;
         return (str);
     }
 }
--- lesson5/src/backpack/MainClass.java
+++ lesson5/src/backpack/MainClass.java
@@ -4,20 +4,16 @@ import java.util.ArrayList;
 
 class MainClass {
     public static void main(String[] args) {
-        ArrayList<Thing> cArr = new ArrayList<>();
-        ArrayList<Thing> mArr = new ArrayList<>();
-        //заполнили массив вещами
-        Thing[] arr = new Thing[5];
-        arr[0] = new Thing("pot", 1, 3);
-        arr[1] = new Thing("book", 1, 6);
-        arr[2] = new Thing("binoculars", 5, 5);
-        arr[3] = new Thing("aid_kit", 4, 7);
-        arr[4] = new Thing("notebook", 5, 4);
+        ArrayList <Thing> arr = new ArrayList<>();
+        arr.add(new Thing("pot", 1, 3));
+        arr.add(new Thing("book", 1, 6));
+        arr.add(new Thing("binoculars", 7, 5));
+        arr.add(new Thing("aid_kit", 4, 7));
+        arr.add(new Thing("notebook", 5, 4));
 
-
-        Backpack bp = new Backpack(16); //рюкзак
-        //bp.pickupThing(arr, 0, 0, 0, 0 , cArr, mArr, 0, 1);
-        bp.pickupThing2(arr, 0, 0, cArr, mArr);  //через цикл сделано
+        Backpack bp = new Backpack(5); //рюкзак
+        //bp.pickupThing2(arr, 0, 0, cArr, mArr);  //через цикл сделано
+        bp.pickupThing(arr);
 
         System.out.println(bp);
     }
